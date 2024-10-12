65b1b4f7dcd7e6303bad9af02fa5c6dee07349ee
ksfegade <138423361+ksfegade@users.noreply.github.com>
2024-10-12 17:52:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
  
 # Access the API key from Streamlit secrets
-openai.api_key = st.secrets["OPENAI_API_KEY"]
+openai.api_key = st.secrets["OPEN_API_KEY"]
  
 # Function to generate text with continuous stream
 def generate_text(prompt):
